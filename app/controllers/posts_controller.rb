dc4a59b3f754504cbb62176625f1de9b75b4ba78
BlueAccords <myoldemailname@gmail.com>
2015-07-29 06:59:42 -0700

    added foreign key to post model, basic post creation form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,17 @@
 class PostsController < ApplicationController
+	before_action :is_signed_in?, only: [:new, :create]
+
+	def new
+		@post = Post.new
+	end
+
+	def create
+
+	end
+
+	def is_signed_in?
+		unless signed_in?
+			redirect_to login_path
+		end
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   get    'login', to: 'sessions#new'
   post   'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
-
-  root 'sessions#new'
+  resources :posts, only: [:new, :create, :index]
+  root 'posts#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
