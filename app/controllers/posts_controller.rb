8f248178d1fa32f2b5934e595ebbc1595c2ce14c
BlueAccords <myoldemailname@gmail.com>
2015-07-29 07:18:38 -0700

    Added index view, create post action, index action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,24 @@
 class PostsController < ApplicationController
 	before_action :is_signed_in?, only: [:new, :create]
 
+	def index
+		@posts = Post.all.order(created_at: :desc)
+	end
+
 	def new
 		@post = Post.new
 	end
 
 	def create
-
+		@post = Post.new(post_params)
+		@post.user_id = current_user.id
+		if @post.save
+			flash[:success] = "Post sucessfully made"
+			redirect_to root_path
+		else
+			flash[:error] = "Post failed to be made"
+			render 'new'
+		end
 	end
 
 	def is_signed_in?
@@ -14,4 +26,9 @@ class PostsController < ApplicationController
 			redirect_to login_path
 		end
 	end
+
+	private
+		def post_params
+			params.require(:post).permit(:title, :body, :user_id)
+		end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ActiveRecord::Base
 	validates :email, presence: true, uniqueness: true
 	has_secure_password
 	validates :password, presence: true, length: { within: 6..30 }
+	has_many :posts
 
 	def User.new_remember_token
 		SecureRandom.urlsafe_base64
